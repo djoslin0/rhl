aa146bcfea11f3103f3fe7269fe6c52178981dbb
joslin_productions@hotmail.com <joslin_productions@hotmail.com>
2018-04-23 19:49:47 -0700

    refactored player, tracked head and glove, added glowing eyes

--- src/a2/GameEntities/Player.java
+++ src/a2/GameEntities/Player.java
@@ -25,6 +25,8 @@ import java.io.IOException;
 
 public class Player extends GameEntity implements Attackable {
     private SceneNode cameraNode;
+    private SceneNode handNode;
+    private SceneNode headNode;
     private RigidBody body;
     private CharacterController controller;
     private byte playerId;
@@ -60,12 +62,14 @@ public class Player extends GameEntity implements Attackable {
         cameraNode.setLocalPosition(0, 1.5f, 0);
         addResponsibility(cameraNode);
 
+        // attach camera
         if (local) {
             Camera camera = EngineManager.getSceneManager().getCamera("MainCamera");
             cameraNode.attachObject(camera);
             camera.setMode('n');
         }
 
+        // create aiming flare
         SceneNode flareNode = cameraNode.createChildSceneNode(name + "flareNode");
         flareNode.moveForward(3);
         try {
@@ -75,43 +79,74 @@ public class Player extends GameEntity implements Attackable {
             e.printStackTrace();
         }
 
-        if (!local) {
-            try {
-                robo = sm.createSkeletalEntity("robo" + name, "robo.rkm", "robo.rks");
-                robo.loadAnimation("idle", "idle.rka");
-                robo.loadAnimation("run", "run.rka");
-                robo.loadAnimation("sidestep", "sidestep.rka");
-                robo.loadAnimation("jump", "jump.rka");
-                robo.loadAnimation("falling", "falling.rka");
-                robo.loadAnimation("land", "land.rka");
-                robo.loadAnimation("crouch_idle", "crouch_idle.rka");
-                robo.loadAnimation("crouch_walk", "crouch_walk.rka");
-                robo.loadAnimation("crouch_sidestep", "crouch_sidestep.rka");
+        // load character
+        if (!local) { loadCharacter(name); }
 
-                Texture texture = sm.getTextureManager().getAssetByPath("robo_uv.png");
-                TextureState textureState = (TextureState)sm.getRenderSystem().createRenderState(RenderState.Type.TEXTURE);
-                textureState.setTexture(texture);
-                robo.setRenderState(textureState);
-
-                SceneNode roboNode = node.createChildSceneNode(name + "RoboNode");
-                roboNode.setLocalPosition(0, -height, 0);
-                addResponsibility(roboNode);
-                roboNode.attachObject(robo);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
+        // store tick
         lastReceivedTick = TimeManager.getTick();
 
+        // initialize physics
         initPhysics();
     }
 
-    public byte getId() { return playerId; }
-    public byte getSide(){
-        return playerSide;
+    private void loadCharacter(String name) {
+        try {
+            SceneManager sm = EngineManager.getSceneManager();
+
+            // load skeletal model
+            robo = sm.createSkeletalEntity("robo" + name, "robo.rkm", "robo.rks");
+
+            // load animations
+            robo.loadAnimation("idle", "idle.rka");
+            robo.loadAnimation("run", "run.rka");
+            robo.loadAnimation("sidestep", "sidestep.rka");
+            robo.loadAnimation("jump", "jump.rka");
+            robo.loadAnimation("falling", "falling.rka");
+            robo.loadAnimation("land", "land.rka");
+            robo.loadAnimation("crouch_idle", "crouch_idle.rka");
+            robo.loadAnimation("crouch_walk", "crouch_walk.rka");
+            robo.loadAnimation("crouch_sidestep", "crouch_sidestep.rka");
+
+            // load texture
+            Texture texture = sm.getTextureManager().getAssetByPath("robo_uv.png");
+            TextureState textureState = (TextureState)sm.getRenderSystem().createRenderState(RenderState.Type.TEXTURE);
+            textureState.setTexture(texture);
+            robo.setRenderState(textureState);
+
+            // setup character node
+            SceneNode roboNode = node.createChildSceneNode(name + "RoboNode");
+            roboNode.setLocalPosition(0, -height, 0);
+            addResponsibility(roboNode);
+            roboNode.attachObject(robo);
+
+            // track right hand
+            handNode = roboNode.createChildSceneNode(name + "HandNode");
+
+            // track head for glowing eyes
+            headNode = roboNode.createChildSceneNode(name + "HeadNode");
+
+            // create eyes
+            float eyeSpacing = 0.225f;
+            float distFromHead = 0.74f;
+            float eyeHeight = 0.6f;
+            float eyeSize = 0.2f;
+
+            // left eye
+            SceneNode leftEyeNode = headNode.createChildSceneNode(name + "EyeLNode");
+            leftEyeNode.setLocalPosition(eyeSpacing, eyeHeight, distFromHead);
+            Billboard leftEyeFlare = new Billboard(leftEyeNode, eyeSize, eyeSize, "flare1.png", Color.YELLOW);
+            addResponsibility(leftEyeFlare);
+
+            // right eye
+            SceneNode rightEyeNode = headNode.createChildSceneNode(name + "EyeRNode");
+            rightEyeNode.setLocalPosition(-eyeSpacing, eyeHeight, distFromHead);
+            Billboard rightEyeFlare = new Billboard(rightEyeNode, eyeSize, eyeSize, "flare1.png", Color.YELLOW);
+            addResponsibility(rightEyeFlare);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    public boolean isLocal() { return local; }
 
     private void initPhysics() {
         createBody(false);
@@ -120,8 +155,8 @@ public class Player extends GameEntity implements Attackable {
 
     public RigidBody createBody(boolean crouching) {
         javax.vecmath.Vector3f velocity = new javax.vecmath.Vector3f();
-
         if (body != null) {
+            // we're replacing our previous body, remember velocity
             body.getLinearVelocity(velocity);
             PhysicsManager.removeRigidBody(body);
             bodyResponsibility.remove(body);
@@ -129,6 +164,7 @@ public class Player extends GameEntity implements Attackable {
 
         float height = crouching ? crouchHeight : this.height;
         float mass = 70;
+
         PlayerMotionStateController motionState = new PlayerMotionStateController(this.node);
         CapsuleShape collisionShape = new CapsuleShape(0.75f, height);
 
@@ -137,6 +173,7 @@ public class Player extends GameEntity implements Attackable {
         } else {
             body = createBody(mass, motionState, collisionShape);
         }
+
         body.setRestitution(0f);
         body.setFriction(0.1f);
         body.setAngularFactor(0);
@@ -149,21 +186,16 @@ public class Player extends GameEntity implements Attackable {
 
     @Override
     public boolean shouldRegisterCollision() { return true; }
-
     public String listedName() { return "player"; }
     public SceneNode getCameraNode() { return cameraNode; }
     public RigidBody getBody() { return body; }
     public CharacterController getController() { return controller; }
     public SkeletalEntity getRobo() { return robo; }
-
-    @Override
-    public void update(float delta) {
-        super.update(delta);
-        if (robo != null) { robo.update(delta); }
-    }
+    public byte getId() { return playerId; }
+    public byte getSide(){ return playerSide; }
+    public boolean isLocal() { return local; }
 
     public float getPitch() { return (float)cameraNode.getLocalRotation().getPitch(); }
-    public float getYaw() { return (float)node.getLocalRotation().getYaw(); }
 
     public void setPitch(float pitch) {
         Matrix3 cameraNodeRotation = Matrix3f.createFrom(pitch, 0, 0);
@@ -171,10 +203,18 @@ public class Player extends GameEntity implements Attackable {
 
     }
 
+    public float getYaw() { return (float)node.getLocalRotation().getYaw(); }
+
     public void setYaw(byte controls, float yaw) {
         double wrapYawValue = CharacterController.wrapYawFromControl(controls) ? Math.PI : 0;
         Matrix3 nodeRotation = Matrix3f.createFrom(wrapYawValue, yaw, wrapYawValue);
         node.setLocalRotation(nodeRotation);
+    }
+
+    public Vector3 getPosition() {
+        Transform t = new Transform();
+        body.getWorldTransform(t);
+        return Vector3f.createFrom(t.origin);
     }
 
     public void setPosition(Vector3 position) {
@@ -185,12 +225,6 @@ public class Player extends GameEntity implements Attackable {
         t.origin.z = position.z();
         body.proceedToTransform(t);
         body.getMotionState().setWorldTransform(t);
-    }
-
-    public Vector3 getPosition() {
-        Transform t = new Transform();
-        body.getWorldTransform(t);
-        return Vector3f.createFrom(t.origin);
     }
 
     public Vector3 getVelocity() {
@@ -212,4 +246,22 @@ public class Player extends GameEntity implements Attackable {
         if (robo == null) { return; }
         robo.playAnimation(animName, animSpeed, endType, 0, interruptable);
     }
+
+    @Override
+    public void update(float delta) {
+        super.update(delta);
+        if (robo != null) {
+            // update animations
+            robo.update(delta);
+
+            // update tracked hand
+            handNode.setLocalPosition(robo.getBoneModelTransform("hand_R").column(3).toVector3());
+
+            // update tracked head
+            Matrix4 headTransform = robo.getBoneModelTransform("head");
+            headNode.setLocalPosition(headTransform.column(3).toVector3());
+            headNode.setLocalRotation(headTransform.toMatrix3());
+        }
+    }
+
 }
