7a2ad15c8c982403af7107cd62698208d9a0f5c5
joslin_productions@hotmail.com <joslin_productions@hotmail.com>
2018-05-05 23:24:50 -0700

    very slight improvement to AI

--- src/a2/GameEntities/AIPlayer.java
+++ src/a2/GameEntities/AIPlayer.java
@@ -1,3 +1,4 @@
+
 package a2.GameEntities;
 
 import a2.Actions.ActionMove;
@@ -9,6 +10,7 @@ import ray.rml.Vector3f;
 public class AIPlayer extends Player{
     private Vector3 goalPosition;
     private Vector3 puckPosition;
+    private boolean attacking;
     public AIPlayer(byte playerId, boolean local, Player.Team side) {
         super(playerId, local, side);
     }
@@ -16,14 +18,14 @@ public class AIPlayer extends Player{
         super.update(delta);
         puckPosition = EntityManager.getPuck().getNode().getWorldPosition();
         if(this.getSide() == Team.Orange){
-           goalPosition = EngineManager.getSceneManager().getSceneNode("Goalbox0").getWorldPosition();
+            goalPosition = EngineManager.getSceneManager().getSceneNode("Goalbox0").getWorldPosition();
         }else{
             goalPosition = EngineManager.getSceneManager().getSceneNode("Goalbox1").getWorldPosition();
         }
         this.lookAt(puckPosition);
         Vector3 attack = getPosition().sub(puckPosition);
         Vector3 goalPuckDif = puckPosition.sub(goalPosition);
-        Vector3 diff = goalPuckDif.normalize().mult(4f);
+        Vector3 diff = goalPuckDif.normalize().mult(6f);
         Vector3 desiredLocation = goalPuckDif.add(diff).add(goalPosition);
         // lookup player
         Vector3 least = getPosition().sub(puckPosition);
@@ -35,22 +37,23 @@ public class AIPlayer extends Player{
             }
 
         }
-        if (me == false) {
+        if (me == false && puckPosition.x() < 0) {
             desiredLocation = puckPosition.sub(goalPosition).mult(.5f);
         }
+        desiredLocation = Vector3f.createFrom(desiredLocation.x(), getPosition().y(), desiredLocation.z());
         this.lookAt(desiredLocation);
-        if(getPosition().sub(desiredLocation).length()<3f && me==true){
-            this.lookAt(puckPosition);
-            getController().move(ActionMove.Direction.FORWARD,true);
-            getController().attack();
-        }else{
-            if(getPosition().sub(desiredLocation).length()<2f){
-                getController().move(ActionMove.Direction.FORWARD,false);
-                lookAt(puckPosition);
-            }else{
-                getController().move(ActionMove.Direction.FORWARD,true);
-            }
 
+        float accuracy = (puckPosition.x() > -0.4) ? 0.25f : 1;
+        if (attacking) {
+            this.lookAt(puckPosition);
+            getController().move(ActionMove.Direction.FORWARD,false);
+            attacking = false;
+        } else if(getPosition().sub(desiredLocation).length()<accuracy && me==true){
+            this.lookAt(puckPosition);
+            getController().attack();
+            attacking = true;
+        }else{
+            getController().move(ActionMove.Direction.FORWARD,true);
         }
 
     }
